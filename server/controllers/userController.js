4a31f33ebb422030f4ec49d61dbd633459a43b37
Steven Del Rosario <stevendelro@pm.me>
2021-06-18 18:26:26 -0500

    POST requests to root and /login now working with postman

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,28 +1,75 @@
-import axios from 'axios';
 import User from '../models/userModel.js';
+import bcrypt from 'bcryptjs';
 
-export const getUserAndTasks = async (req, res) => {
-  console.log(`getUserAndTasks: `, res.body);
-  res.send('getUserAndTasks');
+export const getUserAndTasks = async (req, res, next) => {
+  const userToRetrieve = req.params.id;
+  try {
+    const signedInUser = await User.find({ id: userToRetrieve });
+    console.log(`users in getUserAndTasks: `, signedInUser);
+  } catch (error) {
+    console.error('ERROR IN getUserAndTasks: ', error.message);
+  }
 };
-export const createUser = async (req, res) => {
-  console.log(`createUser: `, res.body);
-  res.send('createUser');
-};
-export const editUser = async (req, res) => {
-  console.log(`editUser: `, res.body);
-  res.send('editUser');
-};
-export const deleteUser = async (req, res) => {
-  console.log(`deleteUser: `, res.body);
-  res.send('deleteUser');
+export const createUser = async (req, res, next) => {
+  try {
+    // bcrypt async try/catch error handling
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(`${req.body.password}`, salt);
+    try {
+      // mongodb create user try/catch error handling
+      const newUser = await User.create({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+        tasklist: [],
+      });
+      res.locals.newUserId = newUser.id;
+      return next();
+    } catch (error) {
+      // TODO: make a global error handler, send it an error here.
+      console.error('ERROR IN mongoDB createUser: ', error.message);
+    }
+  } catch (error) {
+    // TODO: make a global error handler, send it an error here.
+    console.error('ERROR IN bcrypt password hashing: ', error.message);
+  }
 };
 
-export const editTask = async (req, res) => {
-  console.log(`editTask: `, res.body);
-  res.send('editTask');
+export const loginUser = async (req, res, next) => {
+  try {
+    const foundUser = await User.findOne({ username: req.body.username });
+    const isVerified = await bcrypt.compare(
+      `${req.body.password}`,
+      foundUser.password
+    );
+    if (isVerified) {
+      res.locals.currentlyLoggedIn = {
+        id: foundUser.id,
+        name: foundUser.name,
+        username: foundUser.username,        tasklist: [],
+      };
+    }
+    return next()
+  } catch (error) {
+    console.error('ERROR IN verifyUser, user not found: ', error.message);
+  }
 };
-export const deleteTask = async (req, res) => {
-  console.log(`deleteTask: `, res.body);
-  res.send('deleteTask');
+
+export const editUser = async (req, res, next) => {
+  const userToEdit = req.params.id;
+  res.send();
+};
+export const deleteUser = async (req, res, next) => {
+  const userToDelete = req.params.id;
+  res.send();
+};
+
+export const editTask = async (req, res, next) => {
+  const taskToEdit = req.params.taskId;
+  res.send();
+};
+export const deleteTask = async (req, res, next) => {
+  const taskToDelete = req.params.taskId;
+  res.send();
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,8 +3,16 @@ import * as usersController from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.get('/', usersController.getUserAndTasks);
-router.post('/', usersController.createUser);
+router.post('/', usersController.createUser, (req, res) => {
+  res.status(200).json({ createdUserId: res.locals.newUserId });
+});
+
+router.post('/login', usersController.loginUser, (req, res) => {
+  res.status(200).json({ foundUser: res.locals.currentlyLoggedIn });
+});
+
+router.post('/addTask') // figure out how to add tasks to tasklist with refs
+router.get('/:id', usersController.getUserTasks);
 router.patch('/:id', usersController.editUser);
 router.delete('/:id', usersController.deleteUser);
 
