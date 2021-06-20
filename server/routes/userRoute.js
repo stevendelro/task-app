4b9e7f6c10a4df03018d3883956a24a4a822f3c4
Steven Del Rosario <stevendelro@pm.me>
2021-06-20 08:39:00 -0500

    updated userRoutes to respond after all middleware

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,6 +2,7 @@ import express from 'express';
 import * as usersController from '../controllers/userController.js';
 
 const router = express.Router();
+
 /**
  * createUser requires:
  *  - req.body.name,
@@ -11,21 +12,9 @@ const router = express.Router();
  *  - returns a unique ID
  */
 router.post('/', usersController.createUser, (req, res) => {
-  res.status(200).json({ createdUserId: res.locals.newUserId });
+  res.status(201).json({ createdUserId: res.locals.newUserId });
 });
-/**
- * editUser requires:
- *  - req.query.id
- *  - a req.body consisting of the updates
- *  - returns an updated user obj
- */
-router.post('/edit', usersController.editUser);
-/**
- * deleteUser requires:
- *  - req.query.id
- *  - returns the deleted status, username, and email of deleted user.
- */
-router.delete('/delete', usersController.deleteUser);
+
 /**
  * loginUser requires:
  *  - req.body.username
@@ -35,6 +24,31 @@ router.delete('/delete', usersController.deleteUser);
 router.post('/login', usersController.loginUser, (req, res) => {
   res.status(200).json({ currentlyLoggedIn: res.locals.currentlyLoggedIn });
 });
+
+/**
+ * editUser requires:
+ *  - req.query.id
+ *  - a req.body consisting of the following possible updates:
+ *    - req.body.name
+ *    - req.body.username
+ *    - req.body.email
+ *    - req.body.password
+ *
+ *  - returns an updated user obj
+ */
+router.post('/edit', usersController.editUser, (req, res) => {
+  res.status(200).json({ editedUser: res.locals.foundAndUpdatedUser });
+});
+
+/**
+ * deleteUser requires:
+ *  - req.query.userid
+ *  - returns the deleted status, username, and email of deleted user.
+ */
+router.delete('/delete', usersController.deleteUser, (req, res) => {
+  res.status(200).json({ deletedUser: res.locals.deletedUser });
+});
+
 /**
  * editTask requires an object with these props:
  * NOTE: must send userid as a query param as 'userid'
@@ -55,7 +69,21 @@ router.post('/login', usersController.loginUser, (req, res) => {
  *  - completed: req.body.completed,
  *  - tags: [req.body.tags],
  */
-router.post('/task/edit', usersController.editTask);
+router.post('/task/edit', usersController.editTask, (req, res) => {
+  res.status(200).json({ tasklistWithEdits: res.locals.tasklistWithEdits });
+});
+
+/**
+ * deleteTask requires two items sent as query params:
+ *  - userid: req.query.userid
+ *  - taskid: req.query.taskid
+ */
+router.delete('/task/delete', usersController.deleteTask, (req, res) => {
+  res
+    .status(200)
+    .json({ tasklistWithTaskDeleted: res.locals.tasklistWithTaskDeleted });
+});
+
 /**
  * createTask requires an object with these props:
  * NOTE: must send userId as a query param as 'userid'
@@ -75,13 +103,8 @@ router.post('/task/edit', usersController.editTask);
  *  - completed: req.body.completed,
  *  - tags: [req.body.tags],
  */
-router.post('/task', usersController.createTask);
-/**
- * deleteTask requires two items sent as query params:
- *  - userid: req.query.userid
- *  - taskid: req.query.taskid
- */
-router.delete('/task/delete', usersController.deleteTask);
-
+router.post('/task', usersController.createTask, (req, res) => {
+  res.status(200).json({ tasklistWithNewTask: res.locals.tasklistWithNewTask });
+});
 
 export default router;
