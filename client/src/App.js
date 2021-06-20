4401e3e72b5f39467c38441593dcfcea1c424bf3
Steven Del Rosario <stevendelro@pm.me>
2021-06-20 18:20:18 -0500

    SignUpLogin now renders SignUp and Login components

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
+import SignUpLogin from '../src/pages/SignUpLogin'
 
 function Copyright() {
   return (
@@ -17,13 +17,6 @@ function Copyright() {
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          app js
-        </Typography>
-        <Copyright />
-      </Box>
-    </Container>
+    <SignUpLogin />
   );
 }
--- client/src/pages/SignUpLogin/index.js
+++ client/src/pages/SignUpLogin/index.js
@@ -3,20 +3,11 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
-import SignUp from './SignUp'
-import Login from './Login'
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      Steven Del Rosario {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Copyright from '../../components/Copyright';
+import SignUp from './SignUp';
+import Login from './Login';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -32,21 +23,37 @@ const useStyles = makeStyles(theme => ({
     backgroundSize: 'cover',
     backgroundPosition: 'center',
   },
+  formSide: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 }));
 
 export default function SignUpLoginPage() {
   const classes = useStyles();
-  const userAuthenticated = false
+  const hasAnAccount = false;
 
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        {userAuthenticated ? <Login /> : <SignUp />}
-        <Box mt={5}>
-          <Copyright />
-        </Box>
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        md={5}
+        component={Paper}
+        elevation={6}
+        square
+        className={classes.formSide}>
+        <Container>
+          {hasAnAccount ? <Login /> : <SignUp />}
+          <Box mt={5}>
+            <Copyright />
+          </Box>
+        </Container>
       </Grid>
     </Grid>
   );
