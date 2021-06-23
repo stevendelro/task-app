c19d6cdf0710d185d3979c1e0ad191ee03774fe2
Steven Del Rosario <stevendelro@pm.me>
2021-06-22 20:11:48 -0500

    Dashboard page assembled with baseline components

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MyAppBar from '../../components/MyAppbar';
+import MyDrawer from '../../components/MyDrawer';
+import TaskListDisplay from '../../components/TaskListDisplay/';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-function index() {
+function ResponsiveDrawer(props) {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      display: 'flex',
+    },
+    toolbar: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  }));
+  let drawerWidth = 400;
+  const classes = useStyles();
+  const theme = useTheme();
+  const isLargeScreenUp = useMediaQuery(theme.breakpoints.up('lg'));
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () =>  setMobileOpen(!mobileOpen);
+  if (isLargeScreenUp) drawerWidth = 500;
+
   return (
-    <div>
-      Dashboard
+    <div className={classes.root}>
+      <CssBaseline />
+      <MyAppBar
+        drawerWidth={drawerWidth}
+        handleDrawerToggle={handleDrawerToggle}
+      />
+      <MyDrawer
+        drawerWidth={drawerWidth}
+        mobileOpen={mobileOpen}
+        handleDrawerToggle={handleDrawerToggle}
+      />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <TaskListDisplay />
+      </main>
     </div>
-  )
+  );
 }
 
-export default index
+export default ResponsiveDrawer;
