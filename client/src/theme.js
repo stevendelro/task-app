4c77d29ebf9605d3a0f65ce9025f7fe019771d3e
Steven Del Rosario <stevendelro@pm.me>
2021-06-23 13:21:05 -0500

    added more shades to theme

--- client/src/theme.js
+++ client/src/theme.js
@@ -5,11 +5,15 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
+      light: '#8a8989',
       main: '#6d6c6c',
+      dark: '#4c4b4b',
       contrastText: '#ffffff',
     },
     secondary: {
+      light: '#ab3333',
       main: '#960000',
+      dark: '#690000',
       contrastText: '#ffffff',
     },
     error: {
