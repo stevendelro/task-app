7134b465950624e6463a202863dca2aafb33ca22
Steven Del Rosario <stevendelro@pm.me>
2021-06-29 07:29:57 -0500

    switched from Mongo Atlas to local mongo development

--- server/app.js
+++ server/app.js
@@ -18,12 +18,15 @@ app.use('/user', userRoute);
 const MONGO_SRV =
   'mongodb+srv://steven:thisisapassword@taskapp.1t2az.mongodb.net/task-app?retryWrites=true&w=majority';
 
+const LOCAL_MONGO = 'mongodb://localhost/task-app'
+
 mongoose
-  .connect(MONGO_SRV, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
+  // .connect(MONGO_SRV, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   useFindAndModify: false,
+  // })
+  .connect(LOCAL_MONGO)
   .then(() => console.log('DB CONNECTED'))
   .catch(error => console.error(error.message));
 
