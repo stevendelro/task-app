c56e58bc5b893198f1c8aa60b2e7c76dcd5f8f04
Steven Del Rosario <stevendelro@pm.me>
2021-06-29 08:41:07 -0500

    removed sample tasks

--- client/src/components/TaskListDisplay/index.js
+++ client/src/components/TaskListDisplay/index.js
@@ -7,7 +7,6 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import * as actionCreators from '../../actions/userActionCreators';
 import Task from './Task';
-import { tasks } from './fakeTasks';
 
 const useStyles = makeStyles(theme => ({
   addTaskMessage: {
